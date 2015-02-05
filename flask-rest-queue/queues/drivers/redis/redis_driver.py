19d5293147f4cf6481f3e1c94c8b4a0f26a76adc
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2015-02-05 14:11:11 +0100

    Persistent support is added to the Redis

--- flask-rest-queue/queues/drivers/redis/redis_driver.py
+++ flask-rest-queue/queues/drivers/redis/redis_driver.py
@@ -11,29 +11,45 @@ Queue Driver for Redis
 import redis
 
 class RedisDriver:
-    def __init__(self, host='localhost', port=6379, db=0, password=None):
+    def __init__(self, persistent=False,host='localhost', port=6379, db=0, password=None):
         self.redis_pool = redis.ConnectionPool(host=host,port=port,db=db,password=password)
+        self.persistent = persistent
     def create_connection(self):
-        assert(self.redis_pool is not None)
+        assert self.redis_pool is not None
         self.redis_conn = redis.StrictRedis(connection_pool=self.redis_pool)
     def end_pool_connection(self):
-        assert(self.redis_pool is not None)
+        assert self.redis_pool is not None
         self.redis_pool.disconnect()
     def create_pubsub(self):
-        assert(self.redis_conn is not None)
-        return self.redis_conn.pubsub()
-    def subscriber_listener(self,pubsub,queue_name,callback_func=None,callback_attr=None):
-        assert(pubsub is not None)
-        pubsub.subscribe(queue_name)
-        for message in pubsub.listen():
-            if message['type'] == 'message':
+        assert self.redis_conn is not None
+        self.pubsub = self.redis_conn.pubsub()
+    def subscriber_listener(self,queue_name,callback_func=None):
+        assert queue_name is not None
+
+        if self.persistent is False:
+            assert self.pubsub is not None
+            self.pubsub.subscribe(queue_name)
+            for message in pubsub.listen():
+                if message['type'] == 'message':
+                    #Call Callback Function
+                    print "New Message: %s" % (message)
+                    if callback_func is not None:
+                        callback_func(message['data'])
+        else:
+            while True:
+                message = self.redis_conn.blpop(queue_name)
                 #Call Callback Function
                 print "New Message: %s" % (message)
-                if callback_func is not None and callback_attr is not None:
-                    callback_func(*callback_attr)
+                if callback_func is not None:
+                    callback_func(message[1])
+                
     def publish(self,queue_name,message,callback_func=None,callback_attr=None):
-        assert(self.redis_conn is not None)
-        self.redis_conn.publish(queue_name,message)
-        print "Message Sent: %s" %(message)
+        assert self.redis_conn is not None
+        if self.persistent is False:
+            self.redis_conn.publish(queue_name,message)
+            print "Message Sent: %s" %(message)
+        else:
+            self.redis_conn.rpush(queue_name,message)
+
         if callback_func is not None and callback_attr is not None:
             callback_func(*callback_attr)

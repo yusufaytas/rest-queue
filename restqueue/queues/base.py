32d75924da266a9c5a35434658b7c093a34ed781
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2015-02-07 23:17:12 +0100

    Queue Base Class is updated w.r.t Kombu Connection

--- restqueue/queues/base.py
+++ restqueue/queues/base.py
@@ -8,55 +8,23 @@ Queue Base Class
 
 """
 
-import os
-
-try:
-    import urlparse
-except:
-    import urllib.parse as urlparse
-
-DRIVERS = {
-        'redis' : 'restqueue.queues.drivers.redis_driver',
-        'rabbitmq' : 'restqueue.queues.drivers.rabbitmq_driver',
-        'mongodb' : 'restqueue.queues.drivers.mongodb_driver',
-        'qpid' : 'restqueue.queues.drivers.qpid_driver',
-        'sqs' : 'restqueue.queues.drivers.sqs_driver',
-        'zaqar' : 'restqueue.queues.drivers.zaqar_driver',
-        'nsq' : 'restqueue.queues.drivers.nsq_driver',
-        'memcached' : 'restqueue.queues.drivers.memcached_driver'
-}
+from kombu import Connection
 
 class QueueBase:
     def __init__(self, url):
         self.url = url
     def parse_url(self):
-        """ This method's main idea is taken from the Kenneth Reitz's dj-database-url """
-
-        #TODO: Other Message Queues should be added here
-        urlparse.uses_netloc.append('redis')
-        urlparse.uses_netloc.append('rabbitmq')
-        urlparse.uses_netloc.append('mongodb')
-
-        url = urlparse.urlparse(self.url)
-
-        path = url.path[1:]
-        path = path.split('?', 2)[0]
-
-        engine = url.schema
-        hostname = url.hostname
-        user = url.username
-        password = url.password
-        port = url.port
-
-        self.parsed_url = {'engine':engine, 'hostname':hostname, 'username': user,
-                            'password': password, 'port': port}
+        """ Parses the url and creates connection object """
+        self.connection = Connection(self.url)
     def get_queue_driver(self):
         self.parse_url()
         return DRIVERS[self.parsed_url['engine']]
-    def connect(self):
-        pass
-    def disconnect(self):
-        pass
+    def connect(self, connection_hook=None, connection_hook_params=None):
+        self.connection.connect()
+        connection_hook(*connection_hook_params)
+    def disconnect(self, disconnect_hook=None, disconnect_hook_params=None):
+        self.connection.close()
+        disconnect_hook(*disconnect_hook_params)
     def set_producer(self):
         pass
     def set_consumer(self):

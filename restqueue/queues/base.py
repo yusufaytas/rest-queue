a507dbbc16204fe18706c09c9b6bf0bdb43f0c4d
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2015-02-10 16:05:00 +0100

    Base and Consumer files are updated

--- restqueue/queues/base.py
+++ restqueue/queues/base.py
@@ -4,33 +4,25 @@
 restqueue.queues.base
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Queue Base Class
+Queue Base
 
 """
 
 from kombu import Connection, Producer, Consumer
 
-class QueueBase(object):
-    def __init__(self, url, timeout=None, transport_options=None, is_pool_enabled=False):
-        self.url = url
-        self.timeout = timeout
-        self.transport_options = transport_options
-        self.is_pool_enabled = is_pool_enabled
-    def parse_url(self):
-        """ Parses the url and creates connection object """
-        self.connection = Connection(self.url)
-    def get_queue_driver(self):
-        self.parse_url()
-        return DRIVERS[self.parsed_url['engine']]
-    def connect(self, connection_hook=None, connection_hook_params=None):
-        self.connection.connect()
-        connection_hook(*connection_hook_params)
-    def disconnect(self, disconnect_hook=None, disconnect_hook_params=None):
-        self.connection.close()
-        disconnect_hook(*disconnect_hook_params)
-    def create_producer(self):
-        pass
-    def publish_message(self):
-        pass
-    def create_consumer(self):
-        pass
+def parse_url(url):
+    """ Parses the url and creates connection object """
+    connection = Connection(url)
+    return connection
+def connect_to_queue(connection_hook=None, connection_hook_params=None):
+    connection.connect()
+    connection_hook(*connection_hook_params)
+def disconnect_from_queue(disconnect_hook=None, disconnect_hook_params=None):
+    connection.close()
+    disconnect_hook(*disconnect_hook_params)
+def create_producer():
+    pass
+def publish_message():
+    pass
+def create_consumer():
+    pass
--- restqueue/queues/consumer.py
+++ restqueue/queues/consumer.py
@@ -8,17 +8,16 @@ Queue Consumer Class
 
 """
 
-from kombu import Consumer
+from kombu import ConsumerMixin, Queue
 
-class QueueConsumer(object):
-	"""QueueConsumer is consumer class definition for Queues"""
-	def __init__(self, connection):
+class QueueConsumer(ConsumerMixin):
+	"""QueueConsumer is consumer class definition for Queues that extends the ConsumerMixin"""
+	def __init__(self, connection, queue_name, routing_key="", no_ack=False):
 		self.connection = connection
-	def create_consumer(self):
-		pass
-	def start_consumer(self):
-		pass
-	def on_message(self):
+		self.queue = Queue(queue_name,routing_key=routing_key)
+	def get_consumers(self, Consumer, channel):
+		return [Consumer(queues=[self.queue],callback=[self.on_message_received])]
+	def on_message_received(self, body, message):
 		pass
 	def decode_error(self):
 		pass

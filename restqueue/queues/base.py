8338a27931d7f3ea77d8dcd5acaf9c9d2f404978
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2015-02-10 16:52:50 +0100

    Get Connection Errors are added into the Base file

--- restqueue/queues/base.py
+++ restqueue/queues/base.py
@@ -14,9 +14,11 @@ def parse_url(url):
     """ Parses the url and creates connection object """
     connection = Connection(url)
     return connection
-def connect_to_queue(connection_hook=None, connection_hook_params=None):
+def connect_to_queue(connection,connection_hook=None, connection_hook_params=None):
     connection.connect()
     connection_hook(*connection_hook_params)
-def disconnect_from_queue(disconnect_hook=None, disconnect_hook_params=None):
+def disconnect_from_queue(connection,disconnect_hook=None, disconnect_hook_params=None):
     connection.close()
     disconnect_hook(*disconnect_hook_params)
+def get_connection_errors():
+    return connection.get_connection_errors
